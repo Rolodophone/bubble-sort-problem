b221b0df460e362887c81187b2f8eb0837174ac2
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-03-25 15:24:41 +0000

    final commit

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-def logDebug(msg: str) -> None:
-	print(msg)
-
-
 def swapValues(theList: list, firstIndex: int) -> None:
 	firstValue = theList[firstIndex]  # store first value
 	theList[firstIndex] = theList[firstIndex + 1]  # replace first value with second value
 	theList[firstIndex + 1] = firstValue  # replace second value with stored first value
 
 
-def bubbleSort(theList: list) -> None:
+def bubbleSort(theList: list) -> int:
 	listLen = len(theList)
 	swappedInThisPass = False
 	swaps = 0
@@ -19,11 +15,20 @@ def bubbleSort(theList: list) -> None:
 			if theList[i] > theList[i + 1]:
 				swapValues(theList, i)
 				swaps += 1
-				logDebug(f"swap #{swaps}: {theList}")
+				# print(f"swap #{swaps}: {theList}")
 				swappedInThisPass = True
 
 		if not swappedInThisPass:
 			break
 
+	return swaps
 
-bubbleSort([1, 4, 3, 2])
+
+# test bubble sort with different worse-case lists
+ns = range(4, 100)
+print("n\tswaps")
+
+for n in ns:
+	thisList = list(range(n, 0, -1))  # list of n down to 1 step 1 (as the 0 is exclusive)
+	swaps = bubbleSort(thisList)
+	print(f"{n}\t{swaps}")
